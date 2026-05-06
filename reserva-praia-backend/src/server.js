53b4db19fb1cdb6a1108623b8a0686b64d87ac72
Bruno Rocha Brito <brunobrunorochabrito@gmail.com>
2026-05-05 22:18:53 -0300

    banco demo separado

--- reserva-praia-backend/src/server.js
+++ reserva-praia-backend/src/server.js
@@ -6,26 +6,22 @@ const cors = require('cors');
 
 const app = express();
 
-// Libera acesso total para o App
 app.use(cors({ origin: '*', methods: ['GET', 'POST', 'DELETE', 'PUT'] }));
 app.use(express.json());
 
-// A URI virá do arquivo .env ou do painel da hospedagem
 const uri = process.env.MONGO_URI || "mongodb://brunorochabritto94_db_user:Xtrab2221@ac-vfbmvqe-shard-00-00.mllnxld.mongodb.net:27017,ac-vfbmvqe-shard-00-01.mllnxld.mongodb.net:27017,ac-vfbmvqe-shard-00-02.mllnxld.mongodb.net:27017/?ssl=true&replicaSet=atlas-n96reb-shard-0&authSource=admin&appName=Cluster0";
 
-mongoose.connect(uri)
-  .then(() => console.log("✅ AGORA FOI! MongoDB Conectado na Nuvem"))
+mongoose.connect(uri, { dbName: 'reserva_demo' })
+  .then(() => console.log("✅ MongoDB Demo Conectado!"))
   .catch((err) => console.log("❌ Erro ao conectar banco:", err.message));
 
-// Suas rotas
 const reservaRoutes = require('./routes/reserva.routes');
 app.use('/reservas', reservaRoutes);
 
 app.get("/", (req, res) => {
-  res.send("🚀 SERVIDOR RODANDO E PRONTO!");
+  res.send("🚀 SERVIDOR DEMO RODANDO!");
 });
 
-// IMPORTANTE: Mudei para process.env.PORT para funcionar na hospedagem
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Servidor ON na porta ${PORT}`);
